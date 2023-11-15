398b6f5d43a8b1e23968cbea8c7e6aa7b8d9e16c
Abiria <abiria.dev@gmail.com>
2023-11-15 16:41:08 +0900

    docs: implement basic example

--- examples/js-highlighter.rs
+++ examples/js-highlighter.rs
@@ -1,1 +1,37 @@
-fn main() {}
+use std::{fs::read_to_string, path::PathBuf};
+
+use clap::Parser;
+use swc_common::{BytePos, Span};
+use swc_ecma_parser::{lexer::Lexer, token::TokenAndSpan, StringInput};
+
+#[derive(Parser)]
+struct Args {
+	src: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+	let args = Args::parse();
+
+	let src = read_to_string(args.src)?;
+
+	let lexer = Lexer::new(
+		Default::default(),
+		Default::default(),
+		StringInput::new(
+			&src,
+			BytePos(0),
+			BytePos(src.len() as u32),
+		),
+		None,
+	);
+
+	for TokenAndSpan {
+		span: Span { lo, hi, .. },
+		..
+	} in lexer
+	{
+		println!("{} {} {}", lo.0, hi.0, "#ff0000");
+	}
+
+	Ok(())
+}
